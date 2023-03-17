1b92a36b275b053b48e2510a8c46b63591f898a3
Akshay Meher <akshay_111meher@hotmail.com>
2023-03-17 14:05:39 +0400

    fix zero receiver stake in handle tickets issue

---
+++ src/maps/ClusterSelector.ts
@@ -0,0 +1,20 @@
+import { ClusterSelected } from "../../generated/templates/ClusterSelector/ClusterSelector";
+import { SelectedCluster, Selector } from "../../generated/schema";
+
+export function handleClusterSelected(event: ClusterSelected): void {
+    let selector = Selector.load(event.address.toHexString());
+    if (selector) {
+        let id = selector.networkId.toHexString() + "#" + event.params.epoch.toHexString() + "#" + event.params.cluster.toHexString();
+        let selectedClusterData = SelectedCluster.load(id);
+
+        if (selectedClusterData == null) {
+            selectedClusterData = new SelectedCluster(id);
+            selectedClusterData.epoch = event.params.epoch;
+            selectedClusterData.address = event.params.cluster.toHexString();
+            selectedClusterData.network = selector.networkId.toHexString();
+            selectedClusterData.save();
+        }
+    } else {
+        throw new Error("Selector is not saved in the SG");
+    }
+}
